149c64f6d023b4df4e4d5fcd70e06528eed4dfdd
Nelson Amador <nelson.amador@southworks.com>
2019-04-10 12:22:13 -0300

    BABY STEPS

--- program.js
+++ program.js
@@ -1,1 +1,8 @@
-console.log('HELLO WORLD');
+//console.log(process.argv);
+let sum = Number(0);
+for(var i = 2; i < process.argv.length; i++)
+{
+    var num = Number(process.argv[i]);
+    sum += num;
+}
+console.log(sum);
