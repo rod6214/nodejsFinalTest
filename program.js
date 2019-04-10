b93c9a5c76d92ce35ad0024cc04d65aaec34db59
Nelson Amador <nelson.amador@southworks.com>
2019-04-10 15:56:53 -0300

    HTTP COLLECT

--- program.js
+++ program.js
@@ -1,11 +1,13 @@
+var bl = require('bl');
 var http = require('http');
 
 http.get(process.argv[2], function(res) {
-  res.setEncoding('utf8')
-  res.on('data', function(data){
+  res.setEncoding('utf8');
+  res.pipe(bl(function (err, data) {
+      console.log(data.length);
+      console.log(data.toString());
+  }))
+  /* res.on('data', function(data){
       console.log(data);
-  });
+  }); */
 });
-
-
-//while(true);
