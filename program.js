0b7ef36201bddb7026a04bb3e54524f4fcf55970
Nelson Amador <nelson.amador@southworks.com>
2019-04-10 12:47:20 -0300

    MY FIRST I/O

--- program.js
+++ program.js
@@ -1,8 +1,5 @@
-//console.log(process.argv);
-let sum = Number(0);
-for(var i = 2; i < process.argv.length; i++)
-{
-    var num = Number(process.argv[i]);
-    sum += num;
-}
-console.log(sum);
+var fs = require('fs');
+var buffer = fs.readFileSync(process.argv[2]);
+var str = buffer.toString();
+var lines = str.split('\n');
+console.log(lines.length - 1);
