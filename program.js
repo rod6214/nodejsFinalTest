543ccda8d2579d7e828ddb574e3e58175fcd469d
Nelson Amador <nelson.amador@southworks.com>
2019-04-12 01:53:11 -0300

    Fetch Json

--- program.js
+++ program.js
@@ -1,27 +1,10 @@
-var promise1 = getPromise1();
-var promise2 = getPromise2();
 
-function all(p1, p2){
-    var counter = 0;
-    var list = [];
-    return new Promise((fulFill, reject)=>{
-        
-        p1.then((x) => {
-            list.push(x);
-            if(counter > 0){
-                fulFill(list);
-            }
-            counter++
-        });
-        p2.then((y) => {
-            list.push(y);
-            if(counter > 0){
-                fulFill(list);
-            } 
-            counter++
-        });
-        
-    });
-}
+var http = require('./node_modules/q-io/http');
 
-all(promise1, promise2).then(console.log);
+var proReader = http.read('http://localhost:1337');
+
+proReader.then((json)=>{
+    console.log(JSON.parse(json));
+});
+
+
