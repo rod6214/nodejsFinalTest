d1baeb59bd0bcd1c4d98d7adb2a00e6d29f81724
Nelson Amador <nelson.amador@southworks.com>
2019-04-12 00:02:20 -0300

    Throw an error

--- program.js
+++ program.js
@@ -1,17 +1,19 @@
 
-
-var promise = new Promise((fulfill, reject)=>{
-    fulfill('MANHATTAN');
-}).then(promiseChainOff).then(console.log);
-
-
-
-function promiseChainOff(title){
-    return Promise.resolve(title).then(attachTitle);
+function parsePromised(json){
+    
+    return new Promise((fulFill, reject)=>{
+        
+        try{
+            json = JSON.parse(process.argv[2]);
+            fulFill(json);
+        }catch(err){
+            reject(error);
+        }
+    });
 }
 
-secondP.then(console.log);
-
-function attachTitle(title){
-    return 'DR. ' + title;
+function onReject(error){
+    console.log(error.message);
 }
+
+parsePromised(process.argv[2]).then(null, onReject);
