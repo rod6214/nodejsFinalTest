86a2f93c5c3d44dc868157a721c8b0748b48ae5c
Nelson Amador <nelson.amador@southworks.com>
2019-04-12 02:15:49 -0300

    Do some work

--- program.js
+++ program.js
@@ -1,10 +1,28 @@
 
 var http = require('./node_modules/q-io/http');
 
-var proReader = http.read('http://localhost:1337');
+var proSession = http.read('http://localhost:7000');
 
-proReader.then((json)=>{
-    console.log(JSON.parse(json));
-});
+
+var user = function(srv){
+    return new Promise((fulFill, reject)=>{
+        srv.then((id) => {
+            let proDb = http.read('http://localhost:7001/' + id);
+            proDb.then((user)=>{
+                fulFill(JSON.parse(user));
+            });
+        });
+    });
+}
+
+user(proSession).then(onFulFill);
+
+function onFulFill(data){
+    console.log(data);
+}
+
+
+
+
 
 
